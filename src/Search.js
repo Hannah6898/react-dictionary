673d0f9de8d13f61e5629b25f34dae1e47a1d110
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-19 20:53:27 +0100

    Imported dictionary api, and made api call

--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,45 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
   const [keyword, setKeyWord] = useState(null);
+  const [defintion, setDefintion] = useState(null);
+
+  function handleSearch(response) {
+    console.log(response.data[0]);
+    let def = response.data[0];
+    setDefintion({
+      word: def.word,
+      phonetics: def.phonetics[0].text,
+    });
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleSearch);
+    console.log(apiUrl);
   }
 
   function handleChange(event) {
     setKeyWord(event.target.value);
   }
+
   return (
     <main className="Search">
       <form onSubmit={search}>
-        <div class=" search-bar input-group ">
+        <div className=" search-bar input-group ">
           <input
             type="search"
-            class="form-control"
+            className="form-control"
             placeholder="Search for a word"
             aria-label="Search bar"
             aria-describedby="button-addon2"
             onChange={handleChange}
           ></input>
           <button
-            class="btn btn-outline-light"
+            className="btn btn-outline-light"
             type="button"
             id="button-addon2"
           >
@@ -32,6 +47,9 @@ export default function Search() {
           </button>
         </div>
       </form>
+
+      <h1>{defintion.word}</h1>
+      <h1>{defintion.phonetics}</h1>
     </main>
   );
 }
