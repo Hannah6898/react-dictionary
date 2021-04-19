a224ec384e1c364100484d665293370d23997755
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-19 20:11:08 +0100

    Added event listener to search bar

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  const [keyword, setKeyWord] = useState(null);
+  function search(event) {
+    event.preventDefault();
+    alert(`${keyword}`);
+  }
+
+  function handleChange(event) {
+    setKeyWord(event.target.value);
+  }
   return (
     <main className="Search">
-      <form>
+      <form onSubmit={search}>
         <div class=" search-bar input-group ">
           <input
-            type="text"
+            type="search"
             class="form-control"
             placeholder="Search for a word"
             aria-label="Search bar"
             aria-describedby="button-addon2"
+            onChange={handleChange}
           ></input>
           <button
             class="btn btn-outline-light"
