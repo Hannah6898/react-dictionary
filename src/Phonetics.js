a26e66e9a9bdf362611c88219cd776c95afba853
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-27 18:21:45 +0100

    Made audio player responsive

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,13 +3,20 @@ import "./Phonetics.css";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
+  function playSound() {
+    return null;
+  }
   return (
     <div className="Phonetics">
       <span className="text">
         <em>{props.phonetics.text}</em>
       </span>
       <span className="audio-player">
-        <ReactAudioPlayer src={props.phonetics.audio} onPlay controls />
+        <ReactAudioPlayer
+          src={props.phonetics.audio}
+          onPlay={playSound()}
+          controls
+        />
       </span>
     </div>
   );
